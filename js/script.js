e9cad06abd4251ad8e08eddd8a9f32bcf94db287
Heitor <115489905+Astr0ns@users.noreply.github.com>
2023-05-25 03:32:18 +0000

    updade-MaisSports

--- js/script.js
+++ js/script.js
@@ -43,13 +43,4 @@ class MobileNavbar {
   );
   mobileNavbar.init();
 
-  let contador = 1;
-  setInterval( function(){
-    document.getElementById('slide' + contador).checked = true;
-    contador ++;
-
-    if (contador > 5) {
-        contador = 1;
-    }
-
-  },3000)
+ 

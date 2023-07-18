469ca1e3d38f17b298bf2f45d810b440be677430
Heitor <115489905+Astr0ns@users.noreply.github.com>
2023-07-18 21:33:40 +0000

    updade

---
+++ js/valid.js
@@ -0,0 +1,39 @@
+//validação do codigo para inputs
+
+var email = document.forms['form']['email'];
+var senha = document.forms['form']['senha'];
+
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
+
+email.addEventListener('textInput', email_Verify);
+senha.addEventListener('textInput', pass_Verify);
+
+function validated(){
+    if(email.value.length < 9) {
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
+    }
+    if(senha.value.length < 9) {
+        senha.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        senha.focus();
+        return false;
+    }
+}
+function email_Verify(){
+    if (email.value.legth >= 8){
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+    }
+}
+function pass_Verify(){
+    if (senha.value.legth >= 5){
+        senha.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
+}

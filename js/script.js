c488495272de95ad8e76bee5a541f1a6cf3ba198
Heitor <115489905+Astr0ns@users.noreply.github.com>
2023-08-29 20:07:03 +0000

    updade for lasted version

--- js/script.js
+++ js/script.js
@@ -64,3 +64,23 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li",
 );
 mobileNavbar.init();
+
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 3){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}

ded11c52b978fc4529f8242ea523e0dd040ee115
Heitor <115489905+Astr0ns@users.noreply.github.com>
2023-10-04 18:46:49 +0000

    gag

--- js/valid.js
+++ js/valid.js
@@ -1,47 +1,19 @@
-//validação do codigo para inputs
-
-var email = document.forms['form']['email'];
-var senha = document.forms['form']['senha'];
-
-var email_error = document.getElementById('email_error');
-var pass_error = document.getElementById('pass_error');
-
-// email.addEventListener('textInput', email_Verify);
-// senha.addEventListener('textInput', pass_Verify);
-
-function validated(){
-    if(email.value.length < 8) {
-        email.style.border = "1px solid red";
-        email_error.style.display = "block";
-        email.focus();
-        return false;
-    }
-    if(senha.value.length < 5) {
-        senha.style.border = "1px solid red";
-        pass_error.style.display = "block";
-        senha.focus();
-        return onload;
-    }
-
-    if((email.value.legth >= 8) && (senha.value.legth >= 8)){
-        email_error.style.display = "none";
-        return ;
-    }
-
-}
-
-
-// function email_Verify(){
-//     if (email.value.legth >= 8){
-//         email.style.border = "1px solid silver";
-//         email_error.style.display = "none";
-//         return true;
-//     }
-// }
-// function pass_Verify(){
-//     if (senha.value.legth >= 5){
-//         senha.style.border = "1px solid silver";
-//         pass_error.style.display = "none";
-//         return true;
-//     }
-// }
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()

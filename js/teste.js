78f4e56884b6725b588769dd9fe1b8babd49ac04
Heitor <115489905+Astr0ns@users.noreply.github.com>
2023-10-19 05:36:31 +0000

    profile-page-created-and-logics(MASSIVE UPDD)

---
+++ js/teste.js
@@ -0,0 +1,25 @@
+function saveData() {
+    let email, password;
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+
+    let user_records = new Array();
+    user_records = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : []
+    if (user_records.some((v) => {
+        return v.email == email && v.password == password
+    })) {
+        alert("login successfull")
+        let current_user = user_records.filter((v) => {
+            return v.email == email && v.password == password
+        })[0]
+
+        localStorage.setItem("email", current_user.email);
+        localStorage.setItem("name", current_user.name);
+        window.location.href = "profile.html";
+
+    }
+    else {
+        alert("login fall");
+    }
+
+}
